11e23098d97e38b894df4349dd2d71ae8728f1e0
marselgray <marselgray@gmail.com>
2020-02-04 09:53:21 -0500

    Update: started button styles

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Image Slider
 
-
+## Technologies Used
+1. HTML5
+2. JavaScript (vanilla)
+3. CSS3
--- scripts.js
+++ scripts.js
@@ -9,16 +9,36 @@ const nextBtn = document.querySelector('#next');
 let counter = 1;
 const size = carouselImages[0].clientWidth;
 
-carouselSlide.style.transform = `translate(${(-size * counter)}px )`;
+carouselSlide.style.transform = `translateX(${(-size * counter)}px )`;
 
-nextBtn.addEventListener('click', ()=>{
+// btn listeners
+nextBtn.addEventListener('click', () => {
+	if(counter >= carouselImages.length - 1) return;
 	carouselSlide.style.transition = `transform 0.4 ease-in-out`;
 	counter++;
-	carouselSlide.style.transform = `translate(${(-size * counter)}px )`;
+	carouselSlide.style.transform = `translateX(${(-size * counter)}px )`;
 })
 
-prevBtn.addEventListener('click', ()=>{
-	carouselSlide.style.transition = `transform 0.4 ease-in-out`;
+prevBtn.addEventListener('click', () => {
+	if(counter <= 0) return;
+	carouselSlide.style.transition = `transform 0.4s ease-in-out`;
 	counter--;
-	carouselSlide.style.transform = `translate(${(-size * counter)}px )`;
+	console.log(counter);
+	carouselSlide.style.transform = `translateX(${(-size * counter)}px )`;
+})
+
+// transition end
+carouselSlide.addEventListener('transitionend', ()=>{
+
+	if(carouselImages[counter].id === 'lastClone'){
+		carouselSlide.style.transition = `none`;
+		counter = carouselImages.length - 2;
+		carouselSlide.style.transform = `translateX(${(-size * counter)}px )`;
+	}
+
+	if(carouselImages[counter].id === 'firstClone'){
+		carouselSlide.style.transition = `none`;
+		counter = carouselImages.length - counter;
+		carouselSlide.style.transform = `translateX(${(-size * counter)}px )`;
+	}
 })
